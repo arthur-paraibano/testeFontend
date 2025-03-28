e33dc2a15254bc0b24f01aaac33a5892ee725214
arthur-paraibano <arthurmik860@gmail.com>
2025-03-28 17:04:08 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './global.css';
+import Login from './pages/login';
+
+export default function App() {
+  return (
+    <div className="login-conteine">
+        <h2>Tetste</h2>
+      <Login />
+    </div>
+  );
+}
---
+++ src/pages/login/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from '../assets/logo.png';
+
+export default function Login() {
+    return (
+        <div className="login-container">
+          <img src={logo} alt="Logo" className="logo" />
+          {/* Formulário de login */}
+        </div>
+      );
+}
